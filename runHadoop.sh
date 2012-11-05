381c8257f6f01584079850641f96d07e46ed6d5e
Geoff Shannon <earthlingzephyr@gmail.com>
2012-11-05 12:34:52 -0800

    Fix runHadoop

--- runHadoop.sh
+++ runHadoop.sh
@@ -13,7 +13,7 @@ $HADOOP_INSTALL/bin/hadoop jar $HADOOP_INSTALL/contrib/streaming/hadoop-0.20.2-s
   -D stream.num.map.output.key.fields=2 \
   -D mapred.text.key.partitioner.options=-k1,1 \
   -D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator \
-  -D mapred.text.key.comparator.options="-k1n, -k2n" \
+  -D mapred.text.key.comparator.options="-k1n -k2n" \
   -input /home/geoff/work/sage/logs/repo/ \
   -output $HADOOP_OUT_DIR \
   -mapper bin/maplogs.sh \
